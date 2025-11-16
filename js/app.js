b814256f61e511393ce661b2827b2bd2539a73e4
RahmanHanifs <hanifsgan@gmail.com>
2025-11-16 15:53:53 +0700

    perbaikan geser map

--- js/app.js
+++ js/app.js
@@ -12,10 +12,13 @@
     "Ocean Current",
   ];
 
-  const map = L.map("map");
+  const map = L.map("map", {
+    worldCopyJump: true,
+  });
   L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
     maxZoom: 19,
     attribution: "&copy; OpenStreetMap contributors",
+    noWrap: false,
   }).addTo(map);
 
   const allLayerGroup = L.layerGroup().addTo(map);
